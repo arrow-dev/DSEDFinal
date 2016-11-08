7effea2867150f690575129bf7946f4b367afeb2
4rrow <4rrow88@gmail.com>
2016-11-09 11:50:24 +1300

    Start building ui for notifications

--- DSEDFinal/App_Start/BundleConfig.cs
+++ DSEDFinal/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace DSEDFinal
 {
@@ -25,7 +24,8 @@ namespace DSEDFinal
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/animate.css"));
         }
     }
 }
